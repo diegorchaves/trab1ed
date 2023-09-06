4bd2b19050ecfec423b47b018f413047f1f9c2a6
João Vitor da Silva <jdalpicio@outlook.com>
2023-09-06 19:03:04 -0300

    Update matriz.h

--- ex4/matriz.h
+++ ex4/matriz.h
@@ -3,3 +3,5 @@ int*** aloca_matriz (int m, int n, int o);
 void imprime_matriz (int m, int n, int o, int*** mat);
 
 void preenche_matriz(int m, int n, int o, int*** mat);
+
+void desaloca_matriz (int m, int n, int*** mat);
