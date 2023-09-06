b04d132fddfdaa5b2e5f99d8f0eab88f528b6c76
João Vitor da Silva <jdalpicio@outlook.com>
2023-09-06 19:02:18 -0300

    Update exercicio4.c

--- ex4/exercicio4.c
+++ ex4/exercicio4.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "matriz.h"
 
-
-
 int main()
 {   
     int m;
@@ -11,16 +9,17 @@ int main()
     int z;
     int ***mat;
 
-    printf("Digite as linhas. \n");
+    printf("Digite a profundidade. \n");
     scanf("%d", &m);
-    printf("Digite as colunas. \n");
+    printf("Digite as linhas. \n");
     scanf("%d", &n);
-    printf("Digite as profundidade. \n");
+    printf("Digite as colunas. \n");
     scanf("%d", &z);
 
     mat = aloca_matriz(m, n, z);
     preenche_matriz(m, n, z, mat);
-    imprime_matriz(m, n, z, mat);  
+    imprime_matriz(m, n, z, mat); 
+    desaloca_matriz(m, n, mat); 
 
     return 0;
 }
